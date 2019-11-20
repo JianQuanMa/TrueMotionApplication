d78c88c2477f51ac7587cc60b7a0a4bad26f3a6f
Jian Ma <jianquanma@gmail.com>
2019-11-20 18:42:00 -0500

    Initial Commit

---
+++ TrueMotionApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrueMotionApplication
+//
+//  Created by Jian Ma on 11/20/19.
+//  Copyright © 2019 Jian Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
